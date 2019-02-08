c2f7972c9360ef97e0d399af9695c845dc0e82e8
Jacob Werbin <jwerbin95@gmail.com>
2019-02-08 15:29:47 -0600

    Completely Refactored Database
    Database now runs basic CRUD operations and no longer has a view based front end, this will be the famework for my react app

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const fs = require('fs')
 //**********************************************************
 
 //**********************************************************
-//Independent Queries to manage database
-const userQuery =
-	'INSERT INTO "User" (user_name, password, email, phone_number, credit_card_number, address) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
-const companyQuery =
-	'INSERT INTO "company" (name, description) VALUES($1, $2) RETURNING *';
-const productQuery =
-	'INSERT INTO "product" (name, company_fk, description, price) VALUES($1, $2, $3, $4) RETURNING *';
-const productsSelect =
-	'SELECT product.product_id, company.name as cname, product.name as pname, product.price, product.description, product.stock FROM product INNER JOIN company ON product.company_fk=company.company_id';
+//Static Queries to manage database
+const users = 'SELECT * FROM "User"'
+const products = 'SELECT * FROM product'
+const companies = 'SELECT * FROM company'
+const carts = 'SELECT * FROM cart'
+
+const insertUser = 'INSERT INTO "User"(user_name, password, email, phone_number, credit_card_number, address) VALUES($1, $2, $3, $4, $5, $6)'
+const insertCompany = 'INSERT INTO company(name, description) VALUES($1, $2)'
+const insertProduct = 'INSERT INTO product(company_fk, name, description, price, stock) VALUES($1, $2, $3, $4, $5)'
 //***********************************************************
 
 //***********************************************************
@@ -33,10 +33,7 @@ const loggedIn = []
 const connectionString = 'postgresql://localhost:5432/E-Commerce';
 const client = new Client({ connectionString });
 
-let values = [];
 let app = express();
-let errorFlag = false;
-let products = [];
 let accessLogStream = fs.createWriteStream(path.join(__dirname, 'server_log.log'), { flags: 'a' })
 //***********************************************************
 
@@ -44,356 +41,175 @@ let accessLogStream = fs.createWriteStream(path.join(__dirname, 'server_log.log'
 //Middleware
 client.connect();
 
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined', { stream: accessLogStream }))
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
 //***********************************************************
 
 //***********************************************************
 //Get requests
-app.get('/', (request, response)=>{
-	response.redirect('/home')
-})
-
-app.get('/home', (request, response) => {
-	response.render('main', { notLogged: false, error: errorFlag });
-});
-
-app.get('/login', (request, response) => {
-	let stopFlag = false
-	for(let i = 0;i<loggedIn.length;i++){
-		if(request.connection.remoteAddress===loggedIn[i].address){
-			stopFlag = true
-			response.render('main', {notLogged: false, error: true})
-		}
-	}
-	if(!stopFlag){
-		response.render('login', { error: errorFlag });
-	}
-});
-
-app.get('/product_catalog', (request, response) => {
-	client
-		.query(productsSelect)
-		.then(result => {
-			products = result.rows;
-		})
-		.then(result=>{
-			response.render('product_catalog', { products });
-		})
-		.catch(error => {
-			console.log(error.stack);
-		});
-});
-
-app.get('/new_user', (request, response) => {
-	response.render('new_user', { error: errorFlag });
-});
-
-app.get('/register_company', (request, response) => {
-	response.render('new_company', { error: errorFlag });
-});
-
-app.get('/add_a_product', (request, response) => {
-	response.render('new_product', { error: errorFlag });
-});
-
-app.get('/user_account', (request, response)=>{
-	let loggedInFlag = false
-	for(let i = 0;i<loggedIn.length;i++){
-		if(loggedIn[i].address===request.connection.remoteAddress)
-			loggedInFlag = true
-	}
-	if(loggedInFlag){
-		for(let user of loggedIn){
-			if(user.address===request.connection.remoteAddress){
-				const userQuery = `SELECT * FROM "User" WHERE user_id=${user.user_id}`
-				client
-					.query(userQuery)
-					.then(result=>{
-						let newCartData = getCartData(user.user_id).then(gcdresult=>{
-							response.render('user_account', {user: result.rows[0], cartData: gcdresult.rows})
-						})
-					})
-			}
-		}
-	}
-	else{
-		response.render('main', {notLogged: true, error: false})
-	}
-})
-
-app.get('/user_account/logout', (request, response)=>{
-	for(let i = 0;i<loggedIn.length;i++){
-		if(request.connection.remoteAddress===loggedIn[i].address){
-			loggedIn.splice(i, 1)
-		}
-	}
-	response.redirect('/home')
-})
-
-app.get('/edit_profile', (request, response)=>{
-	response.render('edit_user')
-})
-//**********************************************************
-
-//**********************************************************
-//post requests
-app.post('/edit_profile', (request, response)=>{
-	for(let user of loggedIn){
-		if(user.address===request.connection.remoteAddress){
-			if(request.body['User Name']!=""){
-				let updateUser = `UPDATE "User" SET user_name='${request.body['User Name']}' WHERE user_id=${user.user_id}`
-				client
-					.query(updateUser)
-					.then(result=>{
-						console.log("Successfully updated user!")
-						response.redirect('/user_account')
-					})
-					.catch(error=>{
-						console.log(error.stack)
-						response.send("Something Went Wrong :(")
-					})
-			}
-			if(request.body['Password']!=""){
-				updateUser = `UPDATE "User" SET password='${request.body['Password']}' WHERE user_id=${user.user_id}`
-				client
-					.query(updateUser)
-					.then(result=>{
-						console.log("Successfully updated user!")
-						response.redirect('/user_account')
-					})
-					.catch(error=>{
-						console.log(error.stack)
-						response.send("Something Went Wrong :(")
-					})
-			}
-			if(request.body['Email Address']!=""){
-				updateUser = `UPDATE "User" SET email='${request.body['Email Address']}' WHERE user_id=${user.user_id}`
-				client
-					.query(updateUser)
-					.then(result=>{
-						console.log("Successfully updated user!")
-						response.redirect('/user_account')
-					})
-					.catch(error=>{
-						console.log(error.stack)
-						response.send("Something Went Wrong :(")
-					})
-			}
-			if(request.body['Phone Number']!=""){
-				updateUser = `UPDATE "User" SET phone_number='${request.body['Phone Number']}' WHERE user_id=${user.user_id}`
-				client
-					.query(updateUser)
-					.then(result=>{
-						console.log("Successfully updated user!")
-						response.redirect('/user_account')
-					})
-					.catch(error=>{
-						console.log(error.stack)
-						response.send("Something Went Wrong :(")
-					})
-			}
-			if(request.body['Credit Card']!=""){
-				updateUser = `UPDATE "User" SET credit_card_number='${request.body['Credit Card']}' WHERE user_id=${user.user_id}`
-				client
-					.query(updateUser)
-					.then(result=>{
-						console.log("Successfully updated user!")
-						response.redirect('/user_account')
-					})
-					.catch(error=>{
-						console.log(error.stack)
-						response.send("Something Went Wrong :(")
-					})
-			}
-			if(request.body['Address']!=""){
-				updateUser = `UPDATE "User" SET address='${request.body['Address']}' WHERE user_id=${user.user_id}`
-				client
-					.query(updateUser)
-					.then(result=>{
-						console.log("Successfully updated user!")
-						response.redirect('/user_account')
-					})
-					.catch(error=>{
-						console.log(error.stack)
-						response.send("Something Went Wrong :(")
-					})
-			}
-		}
-	}
-})
-
-app.post('/login', (request, response) => {
-	let credentials = login(
-		request.body['User Name'],
-		request.body['Password']
-	);
-	client
-		.query(credentials)
-		.then(result => {
-			if (result.rows[0] === undefined) throw Error('User not found');
-			else {
-				let shoppingCart = showCart(result.rows[0].user_id);
-				let next = result;
-				client
-					.query(shoppingCart)
-					.then(result => {
-						response.redirect('user_account');
-					})
-					.catch(error => {
-						console.log(error.stack)
-						response.send('Something Went Wrong :(');
-					});
-					loggedIn.push({address: request.connection.remoteAddress, user_id: result.rows[0].user_id})
-			}
-		})
-		.catch(error => {
-			console.log(error.stack);
-			errorFlag = true;
-			response.render('login', { error: errorFlag });
-			errorFlag = false;
-		});
-});
-
-app.post('/product_catalog', (request, response)=>{
-	let cartData = request.body['cartData']
-	let userId = null;
-	cartData = cartData.slice(0, -1);
-	const cartProductSelect = `SELECT * FROM product WHERE product_id=${cartData}`
-	for(let user of loggedIn){
-		if(user.address === request.connection.remoteAddress)
-			userId = user.user_id
-	}
-	client
-		.query(cartProductSelect)
-		.then(result=>{
-			let nextQuery = result
-			const userCartQuery = `SELECT cart_fk FROM "User" WHERE user_id=${userId}`
-			client
-				.query(userCartQuery)
-				.then(result=>{
-					const addCartQuery = `INSERT INTO "cart_and_product" (product_fk, cart_fk) VALUES(${nextQuery.rows[0].product_id}, ${result.rows[0].cart_fk})`
-					client
-						.query(addCartQuery)
-						.then(result=>{
-						response.redirect('/user_account')
-				})
-				.catch(error=>{
-					console.log(error.stack)
-					response.send("Something Went Wrong :(")
-				})
-			})
-				
-		})
-		.catch(error=>{
-			console.log(error.stack)
-			response.send("Something Went Wrong :(")
-		})
-})
-
-app.post('/add_a_product', (request, response) => {
-	readBody(request, response, productQuery, 'new_product');
-});
-
-app.post('/new_user', (request, response) => {
-	readBody(request, response, userQuery, 'new_user', createCart);
-});
-
-app.post('/register_company', (request, response) => {
-	readBody(request, response, companyQuery, 'new_company');
-});
-
-app.post('/user_account', (request, response)=>{
-	for(let item in request.body){
-		let toRemove = request.body[item].slice(0, -1);
-		let removeFromCartQuery = `DELETE FROM cart_and_product WHERE product_fk=${toRemove}`
-		client
-			.query(removeFromCartQuery)
-			.then(result=>{
-				console.log("Successfully removed item from cart!")
-			})
-			.catch(error=>{
-				console.log(error.stack)
-				response.send("Something Went Wrong :(")
-			})
-	}
-	response.redirect('/home')
-})
-//**********************************************************
-
-//**********************************************************
-//Following function responsible for returning data within a users shopping cart
-//function had to be asynchronus to wait for the function to resolve
-async function getCartData(userKey){
-	let promise = new Promise((resolve, reject)=>{
-	const cartQuery = `SELECT cart_fk FROM "User" WHERE user_id=${userKey}`
-	client
-		.query(cartQuery)
-		.then(result=>{
-			let cart = result.rows[0].cart_fk
-			const cartQuery = `SELECT product.company_fk, product.description, product.name, product.price, product.product_id, product.stock FROM product INNER JOIN cart_and_product ON cart_and_product.product_fk=product.product_id WHERE cart_and_product.cart_fk=${cart}`
-		client
-			.query(cartQuery)
-			.then(result=>{
-				resolve(result)
-			})
-			.catch(error=>{
-				console.log(error.stack)
-			})
-		})
-		.catch(error=>{
-			console.log(error.stack)
-		})
+app.get('/products', async(request, response) => {
+	let result = await queryDB(products, null)
+	let data = []
+	result.rows.map(item=>{
+		data.push(item)
 	})
-	let returnValue = await promise;
-	return returnValue
-}
+	response.json(data)
+});
+
+app.get('/users', async(request, response) => {
+	let result = await queryDB(users, null)
+	let data = []
+	result.rows.map(item=>{
+		data.push(item)
+	})
+	response.json(data)
+});
+
+app.get('/companies', async(request, response)=>{
+	let result = await queryDB(companies, null)
+	let data = []
+	result.rows.map(item=>{
+		data.push(item)
+	})
+	response.json(data)
+})
+
+app.get('/carts', async(request, response)=>{
+	let result = await queryDB(carts, null)
+	let data = []
+	result.rows.map(item=>{
+		data.push(item)
+	})
+	response.json(data)
+});
 //**********************************************************
 
 //**********************************************************
-//SQL Queries nested within functions for custom input
-function login(userName, password) {
-	let userSelect = `SELECT user_id, user_name, email, phone_number, address FROM "User" WHERE user_name='${userName}' AND password='${password}'`;
-	return userSelect;
-}
+//Post Requests
+app.post('/new_product', async(request, response) => {
+	let values = []
+	for(let item in request.body){
+		values.push(request.body[item])
+	}
+	let queried = await queryDB(insertProduct, values)
+	response.send("Successfully Added New product!")
+});
 
-function showCart(userId) {
-	const cartSelect = `SELECT product.name, product.price FROM product, cart_and_product WHERE cart_and_product.cart_fk=${userId} AND product.product_id=cart_and_product.product_fk`;
-	return cartSelect;
-}
+app.post('/new_user', async(request, response) => {
+	let values = []
+	for(let item in request.body){
+		values.push(request.body[item])
+	}
+	let queried = await queryDB(insertUser, values)
+	response.send("Successfully Added New User!")
+});
 
-function createCart(customerId) {
-	client.query(
-		`INSERT INTO "cart" () VALUES() RETURNING *` //Create a new cart when users are created, cart has no columns besides a primary key
-	);
-}
+app.post('/new_company', async(request, response) => {
+	let values = []
+	for(let item in request.body){
+		values.push(request.body[item])
+	}
+	let queried = await queryDB(insertCompany, values)
+	response.send("Successfully Added New Company!")
+});
 //**********************************************************
 
 //**********************************************************
-//Function responsible for reading the HTTP body using body-parser
-function readBody(request, response, query, errorPage, callback) {
-	for (let item in request.body)
-		if (request.body[item] === '') values.push(null);
-		else values.push(request.body[item]);
+//Put Requests
+app.put('/edit_user/:id', async(request, response)=>{
+	let values = []
+	for(let item in request.body){
+		values.push(request.body[item])
+	}
+	const editUser = `PREPARE query(INT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, INT) AS UPDATE "User" SET user_name=$2, password=$3, email=$4, phone_number=$5, credit_card_number=$6, address=$7, cart_fk=$8 WHERE user_id=$1; EXECUTE query(${request.params.id}, '${values[0]}', '${values[1]}', '${values[2]}', '${values[3]}', '${values[4]}', '${values[5]}', ${values[6]}); DEALLOCATE query;`
 	client
-		.query(query, values)
-		.then(result => {
-			if (typeof callback === 'function')
-				callback(result.rows[0].user_id);
-			response.render('main', { notLogged: false, error: errorFlag });
-			values = [];
+		.query(editUser)
+		.then(result=>{
+			response.json(result)
 		})
-		.catch(error => {
-			console.log(error.stack);
-			errorFlag = true;
-			response.render(errorPage, { error: errorFlag });
-			errorFlag = false;
-			values = [];
-		});
+});
+
+app.put('/edit_company/:id', (request, response)=>{
+	let values = []
+	for(let item in request.body){
+		values.push(request.body[item])
+	}
+	const editCompany = `PREPARE query(INT, TEXT, TEXT) AS UPDATE company SET name=$2, description=$3 WHERE company_id=$1; EXECUTE query(${request.params.id}, '${values[0]}', '${values[1]}'); DEALLOCATE query;`
+	client
+		.query(editCompany)
+		.then(result=>{
+			response.json(result)
+		})
+});
+
+app.put('/edit_product/:id', (request, response)=>{
+	let values = []
+	for(let item in request.body){
+		values.push(request.body[item])
+	}
+	const editProduct = `PREPARE query(INT, INT, TEXT, TEXT, NUMERIC, INT) AS UPDATE product SET company_fk=$2, name=$3, description=$4, price=$5, stock=$6 WHERE product_id=$1; EXECUTE query(${request.params.id}, '${values[0]}', '${values[1]}', '${values[2]}', ${values[3]}, ${values[4]}); DEALLOCATE query;`
+	client
+		.query(editProduct)
+		.then(result=>{
+			response.json(result)
+		})
+});
+//**********************************************************
+
+//**********************************************************
+//Delete Requests
+app.delete('/delete_user/:id', (request, response)=>{
+	deleteRow(request.params.id, "\"User\"", response)
+});
+
+app.delete('/delete_company/:id', (request, response)=>{
+	deleteRow(request.params.id, "company", response)
+});
+
+app.delete('/delete_product/:id', (request, response)=>{
+	deleteRow(request.params.id, "product", response)
+});
+
+app.delete('/delete_product_from_cart/:id', (request, response)=>{
+	deleteRow(request.params.id, "cart_and_product", response)
+});
+//**********************************************************
+
+//**********************************************************
+//Function to query the database
+function queryDB(query, values){
+	let promise = new Promise((resolve, reject)=>{
+	if(values===null){
+			client
+				.query(query)
+				.then(result=>{
+					resolve(result)
+			})
+		}
+		else{
+			client
+				.query(query, values)
+				.then(result=>{
+					resolve(result)
+			})
+		}
+	})
+	return promise
+}
+//**********************************************************
+app.use(function (error, request, respose, next) {
+  console.error(error.stack)
+  response.status(500).send('Something Went Wrong :(')
+})
+//**********************************************************
+//SQL query to delete row from table
+function deleteRow(id, table, response){
+	const deleteQuery = `PREPARE query(INT) AS DELETE FROM ${table} WHERE user_id=$1; EXECUTE query(${id}); DEALLOCATE query;`
+	client
+		.query(deleteQuery)
+		.then(result=>{
+			response.json(result)
+		})
 }
 //**********************************************************
 
